9c4f5adef1cc28ec864a0d2e864ff720ab72f775
Kim-hyunbean <95971047+Kim-hyunbean@users.noreply.github.com>
2024-01-03 13:43:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -118,8 +118,8 @@ with tab1:
             #### 거리주소 -> 위도/경도 변환 함수 호출
             lati, longit = geocoding(address)
 
-            display_df = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%96%91%EB%B4%89qhd.csv',encoding="cp949")
-            display_df2 = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%86%8C%EB%B0%A9%EC%84%9C%EC%9C%84%EC%B9%98.csv',encoding="cp949")
+            display_df = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%96%91%EB%B4%89qhd.csv',encoding='utf-8')
+            display_df2 = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%86%8C%EB%B0%A9%EC%84%9C%EC%9C%84%EC%B9%98.csv',encoding='utf-8')
 
             distance = []
             patient = (lati, longit)
@@ -255,8 +255,8 @@ with tab2:
             #### 거리주소 -> 위도/경도 변환 함수 호출
             lati, longit = geocoding(address1)
 
-            display_df = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%96%91%EB%B4%89qhd.csv',encoding="cp949")
-            display_df2 = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%86%8C%EB%B0%A9%EC%84%9C%EC%9C%84%EC%B9%98.csv',encoding="cp949")
+            display_df = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%96%91%EB%B4%89qhd.csv',encoding='utf-8')
+            display_df2 = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%84%9C%EC%9A%B8%EC%8B%9C%EC%86%8C%EB%B0%A9%EC%84%9C%EC%9C%84%EC%B9%98.csv',encoding='utf-8')
 
             distance = []
             patient = (lati, longit)
@@ -381,8 +381,8 @@ with tab2:
 with tab3:
     st.markdown("## 양봉장 밀원 분포지도")
 
-    display_df = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%96%91%EB%B4%89qhd.csv',encoding="cp949")
-    display_df2 = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EA%BD%83%EC%A7%80%EB%8F%84.csv',encoding="cp949")
+    display_df = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EC%96%91%EB%B4%89qhd.csv',encoding='utf-8')
+    display_df2 = pd.read_csv('https://github.com/Kim-hyunbean/streamlit/blob/main/%EA%BD%83%EC%A7%80%EB%8F%84.csv',encoding='utf-8')
 
     distance2 = []
 
